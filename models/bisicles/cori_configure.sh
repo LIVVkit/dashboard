668b1c2fd51788f5aab281dfd4ff0887db69dcee
Michael Kelleher <kelleher.mike@gmail.com>
2020-09-18 13:40:47 -0700

    Update BISICLES scripts for new code locations

--- models/bisicles/cori_configure.sh
+++ models/bisicles/cori_configure.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-
+source $HOME/nightly_test_scripts/bisicles_modules.sh
+export BISICLES_HOME=$CSCRATCH/bisicles
+cd $BISICLES_HOME
 pushd Chombo || exit
 svn cleanup . --remove-unversioned
 svn up
-cd lib/mk || exit
-ln -s local/Make.defs.cori.knl.intel Make.defs.local
-popd || exit
+pushd lib/mk || exit
+ln -sf $BISICLES_HOME/Make.defs.local Make.defs.local
+# ln -s local/Make.defs.cori.knl.intel Make.defs.local
+popd && popd || exit
 
 pushd BISICLES || exit
 svn cleanup . --remove-unversioned
--- models/bisicles/run_test.sh
+++ models/bisicles/run_test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
-pushd BISICLES/CISM-interface/exec2D || exit
+echo "Hostname: "`hostname`
+# pushd BISICLES/CISM-interface/exec2D || exit
+pushd BISICLES/code/exec2D || exit
 driver="$(find . -type f -name "driver2d.*.ex")"
-eval "${driver}" "$1"
+eval "srun -n 1 ${driver}" "$1"
