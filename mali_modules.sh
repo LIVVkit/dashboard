8032da28f760d01441ecd8d0f2930072e3ccf0fa
Michael Kelleher <kelleher.mike@gmail.com>
2020-09-21 08:33:02 -0700

    Re-fix MALI modules after accidentally replacing them

--- mali_modules.sh
+++ mali_modules.sh
@@ -1,16 +1,11 @@
-export NERSC_HOST=`/usr/common/usg/bin/nersc_host`
-module unload PrgEnv-gnu
-module unload craype-haswell
-module load PrgEnv-intel
-module load craype-mic-knl
-module load cray-hdf5-parallel
+module unload intel PrgEnv-intel
+module load PrgEnv-gnu
+module load cmake
+module load boost
 module load cray-netcdf-hdf5parallel
-module load visit
+module load cray-parallel-netcdf
+module load cray-tpsl
+module load python
 
-export PETSC_DIR=/global/common/software/m1041/petsc_install/petsc_knl_intel
-export PETSC_ARCH=""
-export CRAY_ROOTFS=DSL
-export CRAYPE_LINK_TYPE=dynamic
-
-export LD_LIBRARY_PATH=${PYTHON_DIR}/lib:${LD_LIBRARY_PATH}
+export CRAYPE_LINK_TYPE=STATIC
 module list
