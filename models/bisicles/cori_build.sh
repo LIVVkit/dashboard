bd6b71280610d4cc4987edcda693ca8ddd91661b
Michael Kelleher <kelleher.mike@gmail.com>
2020-11-09 13:17:58 -0800

    Streamline env variables for test/build

--- models/bisicles/cori_build.sh
+++ models/bisicles/cori_build.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 source $HOME/dashboard/nightly_scripts/bisicles_modules.sh
-# module load cray-hdf5-parallel
-# module load cray-netcdf-hdf5parallel
-# module load cray-shmem
-# module load visit  # NOTE looks like it's only available via PrgEnv-gnu-VisIt
-# module list
-
-# export PETSC_DIR=/global/homes/m/madams/petsc_install/petsc-cori-knl-opt64-intel
-export PETSC_DIR=/global/common/software/m1041/petsc_install/petsc_knl_intel
-export PETSC_ARCH=
 
 # pushd BISICLES/CISM-interface/exec2D || exit
 # pushd BISICLES/code/exec2D
 pushd BISICLES/code/test
+
 make -j4 all MPI=TRUE USE_PETSC=TRUE DEBUG=FALSE OPT=TRUE
--- models/bisicles/run_test.sh
+++ models/bisicles/run_test.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 echo "Hostname: "`hostname`
-
+source $HOME/dashboard/nightly_scripts/bisicles_modules.sh > modules.log
 # pushd BISICLES/code/exec2D || exit
 # driver="$(find . -type f -name "driver2d.*.ex")"
 # eval "srun -n 1 ${driver}" "$1"
 
 pushd BISICLES/code/test || exit
 eval "srun -n 1 $1.Linux.64.CC.ftn.OPT.MPI.PETSC.ex"
-cat pout.0 || exit
-
-# if grep -E "pass" pout.0
-# then
-#     exit 1
-# else
-#     exit 0
-# fi
+mv pout.0 $1.log
+cat $1.log || exit
+! grep -E "fail" $1.log || exit
