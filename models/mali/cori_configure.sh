43f00a59095d7986aedfe4b4cf52fb7800773db1
Michael Kelleher <kelleher.mike@gmail.com>
2021-03-22 11:58:07 -0700

    Add exits so git commands raise CDash failure

--- models/mali/cori_configure.sh
+++ models/mali/cori_configure.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 if [ ! -d  MPAS-Model ]; then
-    git clone -b landice/develop git@github.com:MPAS-Dev/MPAS-Model.git
+    git clone -b landice/develop git@github.com:MPAS-Dev/MPAS-Model.git || exit
 fi
 
 pushd MPAS-Model || exit
-git clean -fx
-git pull --ff-only
+git clean -fx || exit
+git pull --ff-only || exit
 popd || exit
 
 if [ ! -d compass ]; then
-    git clone https://github.com/MPAS-Dev/compass.git
+    git clone https://github.com/MPAS-Dev/compass.git || exit
 fi
 pushd compass || exit
-git clean -fx
-git pull --ff-only
+git clean -fx || exit
+git pull --ff-only || exit
 popd || exit
