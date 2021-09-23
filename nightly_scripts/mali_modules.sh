2e741deb710b780353f18c5463996cb7820a9a55
Michael Kelleher <kelleher.mike@gmail.com>
2021-09-22 18:27:26 -0700

    Move all module loading into module file for MALI

--- nightly_scripts/mali_modules.sh
+++ nightly_scripts/mali_modules.sh
@@ -1,11 +1,13 @@
 module unload intel PrgEnv-intel
 module load PrgEnv-gnu
-module load cmake
+module load cmake/3.21.3
 module load boost
 module load cray-netcdf-hdf5parallel
 module load cray-parallel-netcdf
 module load cray-tpsl
 # module load python
+module unload craype-hugepages2M
+module load darshan
 
 export CRAYPE_LINK_TYPE=STATIC
 module list
