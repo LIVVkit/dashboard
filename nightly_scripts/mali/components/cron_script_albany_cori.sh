878327028ed199ae99b72e1ac13f2b8288c2b84e
Michael Kelleher <kelleher.mike@gmail.com>
2021-10-07 17:57:37 -0700

    Possible fix for missing libgcc_s on Alb build

--- nightly_scripts/mali/components/cron_script_albany_cori.sh
+++ nightly_scripts/mali/components/cron_script_albany_cori.sh
@@ -12,5 +12,8 @@ if [[ -z "${SCRIPT_DIR}" ]]; then
     SCRIPT_DIR=`pwd`
 fi
 
+# Find the gcc library directory
+gcc_lib=$(dirname $(dirname $(which gcc)))/snos/lib64
+echo "GCC LIB: $gcc_lib"
 LOG_FILE=$BASE_DIR/nightly_log_coriAlbany.txt
-eval "env TEST_DIRECTORY=$BASE_DIR SCRIPT_DIRECTORY=$BASE_DIR ctest -VV -S $SCRIPT_DIR/components/ctest_nightly_albany.cmake" > $LOG_FILE 2>&1
+eval "env LD_LIBRARY_PATH=$gcc_lib:$LD_LIBRARY_PATH TEST_DIRECTORY=$BASE_DIR SCRIPT_DIRECTORY=$BASE_DIR ctest -VV -S $SCRIPT_DIR/components/ctest_nightly_albany.cmake" > $LOG_FILE 2>&1
