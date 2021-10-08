12e1d04b21135b6a55d03e35dc47a9bc6ff707e9
Michael Kelleher <kelleher.mike@gmail.com>
2021-10-07 17:57:09 -0700

    Update modules for MALI build

--- nightly_scripts/mali_modules.sh
+++ nightly_scripts/mali_modules.sh
@@ -1,13 +1,13 @@
 module unload intel PrgEnv-intel
+module load darshan
 module load PrgEnv-gnu
 module load cmake/3.21.3
 module load boost
 module load cray-netcdf-hdf5parallel
 module load cray-parallel-netcdf
 module load cray-tpsl
-# module load python
 module unload craype-hugepages2M
-module load darshan
+# module load python
 
 export CRAYPE_LINK_TYPE=STATIC
 module list
