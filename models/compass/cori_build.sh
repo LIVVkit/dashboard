00914fbf3826fcf2f0d5e1d23c88817529f908a2
Michael Kelleher <kelleher.mike@gmail.com>
2022-03-10 09:00:02 -0800

    Bug fix for compass build / test

--- models/compass/cori_build.sh
+++ models/compass/cori_build.sh
@@ -7,13 +7,19 @@
 # - BASE_DIR_NEW
 # - TEST_ROOT
 
-pushd compass || exit
 export COMPASS_ENV=dev_compass_1.0.0_${COMPASS_MPI}
+export ENV_NAME=dev_compass_1.0.0
 if [ -d $CSCRATCH/.conda/envs/$COMPASS_ENV ]; then
     # Remove old compass env if it exists already (conflicts happened once)
     # when updating...caused environment solving to hang
     $CSCRATCH/.conda/bin/conda env remove -n $COMPASS_ENV
 fi
+
+# Also check for a directory with just the name (no mpi version attached)
+if [ -d $CSCRATCH/.conda/envs/$ENV_NAME ]; then
+    $CSCRATCH/.conda/bin/conda env remove -n $ENV_NAME
+fi
+
 # Also check for the temp env
 if [ -d $CSCRATCH/.conda/envs/temp_compass_install ]; then
     $CSCRATCH/.conda/bin/conda env remove -n temp_compass_install
@@ -23,14 +29,11 @@ fi
 
 # Find and load conda environment
 LOAD_COMPASS_SCRIPT=$(find $TEST_ROOT/compass -iname "load_*compass*.sh")
-source $LOAD_COMPASS_SCRIPT || exit
-
-# source $TEST_ROOT/compass/load_dev_compass_1.0.0_cori-knl_intel_openmpi.sh
-# source $TEST_ROOT/compass/load_${COMPASS_ENV}.sh
+source $LOAD_COMPASS_SCRIPT
 
 # Temporary install so summary e-mails can be sent by this environment
-mamba install -c conda-forge gitpython svn ruamel.yaml -y || exit
-pip install svn pysvn || exit
+conda install -c conda-forge gitpython svn ruamel.yaml -y
+pip install svn pysvn
 
 # Clean up old logs
 rm -f $TEST_DIR_RUN_NEW/case_outputs/*.log
@@ -39,8 +42,10 @@ compass suite \
 --core landice \
 --test_suite full_integration \
 --setup \
---machine cori-knl \
+--config_file compass/machines/cori-knl.cfg \
 --work_dir $TEST_DIR_RUN_NEW \
 --baseline_dir $BASE_DIR_NEW \
 --mpas_model $TEST_ROOT/E3SM/components/mpas-albany-landice \
 --clean || exit
+
+# --machine cori-knl \
